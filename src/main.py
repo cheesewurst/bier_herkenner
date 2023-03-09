1ef3526e37efa458dfce18442a3077bbd570edb4
I462086 <i462086@fhict.nl>
2023-03-09 19:12:59 +0100

    Added counter for amount of objects on video

--- src/main.py
+++ src/main.py
@@ -8,24 +8,27 @@ upper = np.array([35, 255, 255])
 
 cam = cv2.VideoCapture(0) 
 
+
+
 while True:
+    objects_detected = 0
     success, frame = cam.read()
     image = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     mask = cv2.inRange(image, lower, upper)
 
-    contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     if len(contours) != 0:
         for contour in contours:
             if cv2.contourArea(contour) > 500:
                 x, y, w, h = cv2.boundingRect(contour)
                 cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
-                cv2.putText(frame, 'haha BIER', (x + 1, y - 6), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 255), 1)
+                objects_detected += 1
+                print(objects_detected)
+                cv2.putText(frame, f'{objects_detected}', (x + 1, y - 6), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 255), 1)
 
 
-    # cv2.imshow("mask", mask)
     cv2.imshow("Geel herkenner", frame)
-
 
     key = cv2.waitKey(1)
     if key == ord('q') or cv2.getWindowProperty('Geel herkenner', cv2.WND_PROP_VISIBLE) < 1:
