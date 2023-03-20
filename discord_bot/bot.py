d7b2d4d65c625c3b2baa2b49bcb2cd9b003d3e8c
I462086 <i462086@fhict.nl>
2023-03-20 22:11:06 +0100

    Added basic discord bot

--- discord_bot/bot.py
+++ discord_bot/bot.py
@@ -6,15 +6,22 @@ import asyncio
 
 load_dotenv()
 
-TOKEN = os.getenv('DISCORD_TOKEN')
+TOKEN =  os.getenv('DISCORD_TOKEN')
 
 intents = discord.Intents.default()
-intents.message_content = True
+intents.messages = True
 client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
     print("Online and ready for service!")
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    await message.channel.send("pindakaas :)")
 
 async def setup():
     print("Setting up....")
